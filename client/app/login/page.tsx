993d4a8ebfa0e34f866b6cb29f76b5c51d6bc918
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-04-17 17:12:16 +1200

    add: client scaffolds

---
+++ client/app/login/page.tsx
@@ -0,0 +1,4 @@
+export default function Login() {
+    return <main className="bg-blue-400">Login</main>;
+  }
+  
---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h3 className="text-indigo-400 text-xl p-10">Header</h3>
+      <h3 className="text-teal-400 text-xl p-10">Main page</h3>
+      <h3 className="text-indigo-400 text-xl p-10">Footer</h3>
+    </main>
+  );
+}
