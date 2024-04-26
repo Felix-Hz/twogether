9d7d065b1d8cb4b6b17ac4ae4c448dafc909d2ef
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-04-26 22:44:30 +1200

    add: dashboard + components

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,3 +1,25 @@
-export default function DashboardRoute() {
-  return <div>DashboardRoute</div>;
+import {
+  Drawer,
+  CardContainer,
+  HeadlineContainer,
+} from "@/components/dashboard";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function DashboardContainer() {
+  return (
+    <div className="flex flex-col h-screen">
+      <header className=" text-white flex items-center justify-between px-4 py-4 md:px-6">
+        <Drawer />
+        <Avatar className="border-2 border-black">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </header>
+      <Separator />
+      <HeadlineContainer />
+      <Separator />
+      <CardContainer />
+    </div>
+  );
 }
